e0a64a1b87d05fba731acb82cc941c411826affd
Dalila <56134527+Dali-codes@users.noreply.github.com>
2022-10-12 16:47:50 +0200

    Wrapped icons in buttons and added missing alt attributes.

--- script.js
+++ script.js
@@ -41,20 +41,20 @@ function renderPostSections() {
             <section class="post-sections">
                 <div class="container">  
                     <div class="user-info">
-                        <img class="user-avatar" src="${posts[i].avatar}">
+                        <img class="user-avatar" src="${posts[i].avatar}" alt="User avatar">
                         <div>
                             <h2 class="user-full-name bold-text">${posts[i].name}</h2>
                             <p class="location-text">${posts[i].location}</p>
                         </div>    
                     </div>    
                     <div>
-                        <img class="post-img" src="${posts[i].post}">
+                        <img class="post-img" src="${posts[i].post}" alt="Painted portrait of the user">
                     </div>
                     <div class="post-content">
                         <div class="icons">
-                            <img class="icon-img like-icon" src="images/icon-heart.png" alt="icon of a heart"> 
-                            <img class="icon-img" src="images/icon-comment.png" alt="icon to comment on post"> 
-                            <img class="icon-img" src="images/icon-dm.png" alt="icon to dm the user">      
+                            <button class="btn"><img class="icon-img like-icon" src="images/icon-heart.png" alt="heart"></button>
+                            <button class="btn"><img class="icon-img" src="images/icon-comment.png" alt="comment on post"></button> 
+                            <button class="btn"><img class="icon-img" src="images/icon-dm.png" alt="dm the user"></button>      
                         </div>
                         <p class="bold-text likes-el"><span class="likes-count">${posts[i].likes.toLocaleString("en-US")}</span> likes</p>
                         <p class="comments-el"><span class="bold-text">${posts[i].username}</span> ${posts[i].comment}</p>
